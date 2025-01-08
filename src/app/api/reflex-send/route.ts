e9e8b67f3334baa5b41ba54f2f7f56e86f749fdf
Himanshu Arya <160384744+CodeDevArya@users.noreply.github.com>
2025-01-08 14:10:43 +0530

    Update route.ts

--- src/app/api/reflex-send/route.ts
+++ src/app/api/reflex-send/route.ts
@@ -1,20 +1,25 @@
 import { sendReflexVerseEmail } from "@/helpers/sendVerificationEmail";
 
-const corsHeaders = {
-  "Access-Control-Allow-Origin": "*", // Allow all origins, or specify your origin
-  "Access-Control-Allow-Methods": "GET, POST, OPTIONS", // Specify allowed HTTP methods
-  "Access-Control-Allow-Headers": "Content-Type, Authorization", // Specify allowed headers
-};
+export async function POST(request: Request, res: any) {
 
-export async function POST(request: Request) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
   const { email, name, budget, customBudget, videoType, videoLength, socialAccounts } = await request.json();
+
+  if (request.method === 'OPTIONS') {
+    // Respond to preflight request
+    res.status(200).end();
+    return;
+  }
+
 
   if (!email || !name || !budget || !videoType) {
     return new Response(
       JSON.stringify({ message: "Missing required fields", success: false }),
       {
-        status: 400,
-        headers: corsHeaders,
+        status: 400
       }
     );
   }
@@ -38,20 +43,19 @@ export async function POST(request: Request) {
         }),
         {
           status: 500,
-          headers: corsHeaders,
         }
       );
     }
 
     return new Response(
       JSON.stringify({ message: "Success - ReflexVerse email sent", success: true }),
-      { status: 200, headers: corsHeaders }
+      { status: 200 }
     );
   } catch (error) {
     console.error("An unexpected error occurred:", error);
     return new Response(
       JSON.stringify({ message: "Internal server error", success: false }),
-      { status: 500, headers: corsHeaders }
+      { status: 500 }
     );
   }
 }
